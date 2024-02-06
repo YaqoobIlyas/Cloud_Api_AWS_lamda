650381f95a97a5672ec4b3352db45f1f456fae67
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-06 17:39:17 +0500

    first commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+const token = process.env.TOKEN;
+
+app.get("/webhooks", (req, res) => {
+  if (
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == token
+  ) {
+    res.send(req.query["hub.challenge"]);
+  } else {
+    res.sendStatus(400);
+  }
+});
+module.exports.handler = serverless(app);
